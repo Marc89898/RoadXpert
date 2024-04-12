d9ebc2d9b492988d88ab4137377ac2909bb4164e
Yjoubayr <YossefJMT@gmail.com>
2024-04-12 17:02:48 +0200

    env.js

--- RoadXpert-Teacher/ROADXPERTADMIN/Components/StartRouteMap.js
+++ RoadXpert-Teacher/ROADXPERTADMIN/Components/StartRouteMap.js
@@ -21,7 +21,7 @@ import { getTrafficData } from "./api/trafficService";
 // audio amanagment
 import axios from 'axios';
 import { Audio } from 'expo-av';
-import env from '../prompts/env';
+import env from '../env';
 import ms from '../prompts/messagesGPT.json';
 
 const StartRouteMap = () => {
