43c8c508848d0857a8d7c89c7dce899376dd4bd5
Jasskaa <Jaskaranmr18@gmail.com>
2024-04-16 21:04:56 +0200

    MapScreenConfigurations

--- RoadXpert-Teacher/ROADXPERTADMIN/App.js
+++ RoadXpert-Teacher/ROADXPERTADMIN/App.js
@@ -24,7 +24,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator
-        initialRouteName="MyCars"
+        initialRouteName="LoginScreen"
         screenOptions={{
           headerShown: false,
         }}
