e86f4f711e3ee62a49ebe9f534704f420593e00a
Jasskaa <Jaskaranmr18@gmail.com>
2024-04-24 18:37:54 +0200

    Ubuntu login error, resolved

--- RoadXpert-Student/ROADXPERT/Components/LoginScreen.js
+++ RoadXpert-Student/ROADXPERT/Components/LoginScreen.js
@@ -1,16 +1,166 @@
+// import React, { useState } from "react";
+// import { View, StyleSheet, Image, Text } from "react-native";
+// import { Button } from "react-native-paper";
+// import { useNavigation } from "@react-navigation/native";
+// import { Ionicons } from "@expo/vector-icons";
+// import { TextInput } from "react-native-rapi-ui";
+// import { APIService } from "./ApiService";
+// import { isValidDNI, isValidPassword, sha256 } from '../utils/utils.js';
+
+// const LoginScreen = () => {
+//   const [username, setUsername] = useState("");
+//   const [password, setPassword] = useState("");
+//   const [showPassword, setShowPassword] = useState(false); 
+//   const navigation = useNavigation();
+
+//   const handleLogin = async () => {
+//     try {
+//       if (!isValidDNI(username)) {
+//         console.error("El usuario no tiene el formato correcto");
+//         return;
+//       }
+//       var passwordHashed = await sha256(password)
+//       setPassword(passwordHashed)
+//       if (!isValidPassword(passwordHashed)) {
+//         console.error("La contraseña no es válida");
+//         return;
+//       }
+//       const alumns = await APIService.fetchAllAlumns();
+//       const foundAlumn = alumns.find(alumn => alumn.DNI === username && alumn.Contrasenya === passwordHashed);
+//       if (foundAlumn) {
+//         console.log("Inicio de sesión exitoso");
+//         navigation.navigate("NavBar");
+//       } else {
+//         console.error("Usuario o contraseña incorrectos");
+//       }
+//     } catch (error) {
+//       console.error("Error al iniciar sesión:", error.message);
+//     }
+//   };
+  
+
+//   return (
+//     <View style={styles.container}> 
+//       <View style={styles.imageContainer}>
+//         <Image
+//           source={require("../assets/images/SplashScreen/LogoRoadXpert.png")}
+//           style={styles.image}
+//         />
+//       </View>
+//       <View style={styles.contentContainer}>
+//         <View style={styles.textContainer}>
+//           <Text style={styles.greetingText}>Hello.</Text>
+//           <Text style={styles.welcomeText}>Welcome Back</Text>
+//         </View>
+//         <View style={styles.inputsContainer}>
+//           <TextInput
+//             placeholder="Enter your username"
+//             value={username}
+//             onChangeText={(val) => setUsername(val)}
+//             rightContent={
+//                 <Ionicons
+//                   name="person"
+//                   size={20}
+//                   color="#ccc"
+//                   style={styles.eyeIcon}
+//                 />
+//               }
+//           />
+//           <TextInput
+//             placeholder="Enter your password"
+//             value={password}
+//             secureTextEntry={!showPassword}
+//             onChangeText={(val) => setPassword(val)}
+//             rightContent={
+//               <Ionicons
+//                 name={showPassword ? "eye" : "eye-off"}
+//                 size={20}
+//                 color="#ccc"
+//                 style={styles.eyeIcon}
+//                 onPress={() => setShowPassword(!showPassword)}
+//               />
+//             }
+//           />
+//         </View>
+//         <Button style={styles.button} mode="contained" onPress={handleLogin}>
+//           Iniciar Sesión
+//         </Button>
+//       </View>
+//     </View>
+//   );
+// };
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#fff",
+//     justifyContent: "center",
+//     alignItems: "center",
+//     padding: 16,
+//   },
+//   imageContainer: {
+//     marginTop: 30,
+//     alignSelf: "flex-end",
+//   },
+//   image: {
+//     width: 70,
+//     height: 70,
+//     resizeMode: "contain",
+//   },
+//   contentContainer: {
+//     flex: 1,
+//     marginTop: 150,
+//     alignItems: "center",
+//     alignSelf: "stretch",
+//   },
+//   textContainer: {
+//     alignSelf: "flex-start",
+//     marginLeft: 20,
+//   },
+//   inputsContainer: {
+//     gap: 20,
+//     width: "100%",
+//     paddingHorizontal: 20,
+//     marginBottom: 20,
+//   },
+//   greetingText: {
+//     fontSize: 24,
+//     fontWeight: "bold",
+//     marginBottom: 5,
+//   },
+//   welcomeText: {
+//     fontSize: 25,
+//     fontWeight: "bold",
+//     marginBottom: 20,
+//   },
+//   input: {
+//     width: "100%",
+//     marginBottom: 12,
+//     backgroundColor: "#F1F4FF",
+//   },
+//   button: {
+//     backgroundColor: "#1F41BB",
+//     width: "90%",
+//     justifyContent: "center",
+//   },
+// });
+
+// export default LoginScreen;
+
+
+
 import React, { useState } from "react";
-import { View, StyleSheet, Image, Text } from "react-native";
+import { View, StyleSheet, Image, Text, TextInput } from "react-native"; // Agregamos el componente TextInput de React Native
 import { Button } from "react-native-paper";
 import { useNavigation } from "@react-navigation/native";
 import { Ionicons } from "@expo/vector-icons";
-import { TextInput } from "react-native-rapi-ui";
 import { APIService } from "./ApiService";
-import { isValidDNI, isValidPassword, sha256 } from '../utils/utils.js';
+import { isValidDNI, isValidPassword, sha256 } from "../utils/utils.js";
 
 const LoginScreen = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  const [showPassword, setShowPassword] = useState(false); 
+  const [showPassword, setShowPassword] = useState(false);
   const navigation = useNavigation();
 
   const handleLogin = async () => {
@@ -19,14 +169,17 @@ const LoginScreen = () => {
         console.error("El usuario no tiene el formato correcto");
         return;
       }
-      var passwordHashed = await sha256(password)
-      setPassword(passwordHashed)
+      var passwordHashed = await sha256(password);
+      setPassword(passwordHashed);
       if (!isValidPassword(passwordHashed)) {
         console.error("La contraseña no es válida");
         return;
       }
       const alumns = await APIService.fetchAllAlumns();
-      const foundAlumn = alumns.find(alumn => alumn.DNI === username && alumn.Contrasenya === passwordHashed);
+      const foundAlumn = alumns.find(
+        (alumn) =>
+          alumn.DNI === username && alumn.Contrasenya === passwordHashed
+      );
       if (foundAlumn) {
         console.log("Inicio de sesión exitoso");
         navigation.navigate("NavBar");
@@ -37,10 +190,9 @@ const LoginScreen = () => {
       console.error("Error al iniciar sesión:", error.message);
     }
   };
-  
 
   return (
-    <View style={styles.container}> 
+    <View style={styles.container}>
       <View style={styles.imageContainer}>
         <Image
           source={require("../assets/images/SplashScreen/LogoRoadXpert.png")}
@@ -53,34 +205,34 @@ const LoginScreen = () => {
           <Text style={styles.welcomeText}>Welcome Back</Text>
         </View>
         <View style={styles.inputsContainer}>
-          <TextInput
-            placeholder="Enter your username"
-            value={username}
-            onChangeText={(val) => setUsername(val)}
-            rightContent={
-                <Ionicons
-                  name="person"
-                  size={20}
-                  color="#ccc"
-                  style={styles.eyeIcon}
-                />
-              }
-          />
-          <TextInput
-            placeholder="Enter your password"
-            value={password}
-            secureTextEntry={!showPassword}
-            onChangeText={(val) => setPassword(val)}
-            rightContent={
-              <Ionicons
-                name={showPassword ? "eye" : "eye-off"}
-                size={20}
-                color="#ccc"
-                style={styles.eyeIcon}
-                onPress={() => setShowPassword(!showPassword)}
-              />
-            }
-          />
+          <View style={styles.inputContainer}>
+            <TextInput
+              style={styles.input}
+              placeholder="Enter your username"
+              value={username}
+              onChangeText={(val) => setUsername(val)}
+            />
+            <Ionicons
+              name="person"
+              size={18}
+              style={styles.icon}
+            />
+          </View>
+          <View style={styles.inputContainer}>
+            <TextInput
+              style={styles.input}
+              placeholder="Enter your password"
+              value={password}
+              secureTextEntry={!showPassword}
+              onChangeText={(val) => setPassword(val)}
+            />
+            <Ionicons
+              name={showPassword ? "eye" : "eye-off"}
+              size={20}
+              style={styles.icon}
+              onPress={() => setShowPassword(!showPassword)}
+            />
+          </View>
         </View>
         <Button style={styles.button} mode="contained" onPress={handleLogin}>
           Iniciar Sesión
@@ -118,7 +270,7 @@ const styles = StyleSheet.create({
     marginLeft: 20,
   },
   inputsContainer: {
-    gap: 20,
+    gap: 10,
     width: "100%",
     paddingHorizontal: 20,
     marginBottom: 20,
@@ -133,10 +285,23 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     marginBottom: 20,
   },
+  inputContainer: {
+    position: "relative",
+  },
+  icon: {
+    position: "absolute",
+    top: 15,
+    color: "grey",
+    right: 15,
+  },
   input: {
     width: "100%",
+    height: 48,
     marginBottom: 12,
-    backgroundColor: "#F1F4FF",
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: "lightgrey",
+    padding: 10,
   },
   button: {
     backgroundColor: "#1F41BB",
