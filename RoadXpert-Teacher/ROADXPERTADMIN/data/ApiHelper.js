b3fdcb0a5a315a092aace095d6032c0c1454f6eb
Yjoubayr <YossefJMT@gmail.com>
2024-05-06 16:56:10 +0200

    Agregada función fetchPracticasPorAlumno a ApiHelper

--- RoadXpert-Teacher/ROADXPERTADMIN/data/ApiHelper.js
+++ RoadXpert-Teacher/ROADXPERTADMIN/data/ApiHelper.js
@@ -181,7 +181,30 @@ class ApiHelper {
             throw new Error(`Error fetching students for professor: ${error.message}`);
         }
     }
-    
+
+    static async fetchPracticasPorAlumno(alumnoId) {
+        try {
+            const response = await axios.get(`/Practica/Alumn/${alumnoId}`);
+            const data = response.data;
+            // Mapear los datos devueltos a un array de objetos
+            const practicas = data.map(practica => ({
+                id: practica.ID,
+                alumneId: practica.AlumneID,
+                data: practica.Data,
+                estatHoraId: practica.EstatHoraID,
+                horaFi: practica.HoraFi,
+                horaInici: practica.HoraInici,
+                km: practica.Km,
+                professorId: practica.ProfessorID,
+                ruta: practica.Ruta,
+                vehicleId: practica.VehicleID
+            }));
+            return practicas;
+        } catch (error) {
+            console.error('Error fetching practicas:', error);
+            throw error;
+        }
+    }
 }
 
 export default ApiHelper;
